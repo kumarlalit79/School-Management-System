8c280813a9c0d225362bd4561eb5bb5150b0ada8
kumarlalait79 <luckyarya0101@gmail.com>
2026-05-18 22:47:31 +0530

    curd of students

--- app/routes/student.py
+++ app/routes/student.py
@@ -1,13 +1,13 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException, status
 from sqlalchemy.orm import Session
-from app.schemas.student import StudentCreate
+from app.schemas.student import StudentCreate , StudentResponse , StudentUpdate
 from app.models.student import Student
 from app.db.dependencies import get_db
 import bcrypt
 
 router = APIRouter()
 
-@router.post("/")
+@router.post("/", response_model=StudentResponse)
 def create_student(
     student: StudentCreate,
     db: Session = Depends(get_db)
@@ -27,7 +27,71 @@ def create_student(
     db.commit()
     db.refresh(new_student)
     
-    return {
-        "message" : "Student created successfully"
-    }
+    return new_student
+
     
+@router.get("/", response_model=list[StudentResponse])
+def get_students(
+    db: Session = Depends(get_db)
+):
+    students = db.query(Student).all()
+    
+    return students
+
+
+@router.get("/{student_id}", response_model=StudentResponse)
+def get_student_by_id(
+    student_id: int,
+    db: Session = Depends(get_db)
+):
+    student = db.query(Student).filter(Student.id == student_id).first()
+  
+    if not student:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Student not found"
+        )
+    
+    return student
+    
+    
+@router.put("/{student_id}", response_model=StudentResponse)
+def update_student(
+    student_id: int,
+    update_student: StudentUpdate,
+    db: Session = Depends(get_db)
+):
+    student = db.query(Student).filter(Student.id == student_id).first()
+    
+    if not student:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Student not found"
+        )
+    
+    student.name = update_student.name
+    student.email = update_student.email
+    
+    db.commit()
+    db.refresh(student)
+
+    return student
+
+@router.delete("/{student_id}")
+def student_delete(
+    student_id: int,
+    db: Session = Depends(get_db)
+):
+    student = db.query(Student).filter(Student.id == student_id).first()
+    
+    if not student:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Student not found"
+        )
+    
+    db.delete(student)
+    db.commit()
+    return {
+        "message": "Student deleted successfully"
+    }
--- app/schemas/student.py
+++ app/schemas/student.py
@@ -4,3 +4,18 @@ class StudentCreate(BaseModel):
     name: str
     email: EmailStr
     password: str
+    
+    
+class StudentResponse(BaseModel):
+    id: int
+    name: str
+    email: EmailStr
+    role: str
+    
+    class Config:
+        from_attributes = True
+        
+        
+class StudentUpdate(BaseModel):
+    name: str
+    email: EmailStr
